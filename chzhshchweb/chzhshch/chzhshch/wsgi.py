9e86375a9a4af1988d30cf945a2c6666416b3f93
baileicheng <baileicheng@exfresh.com.cn>
2017-12-12 14:56:33 +0800

    [mod] project name

--- chzhshchweb/chzhshch/chzhshch/wsgi.py
+++ chzhshchweb/chzhshch/chzhshch/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for chzhshchweb project.
+WSGI config for chzhshch project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chzhshchweb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chzhshch.settings")
 
 application = get_wsgi_application()
