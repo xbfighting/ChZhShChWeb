3a4afe0c1cfd9153617e8523f2096b9589b6a128
baileicheng <baileicheng@exfresh.com.cn>
2017-12-12 18:01:08 +0800

    [mod] t01-t05

--- chzhshch/polls/views.py
+++ chzhshch/polls/views.py
@@ -1,4 +1,23 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Question
+from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
