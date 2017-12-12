1aa30d5cd4ca20d0dec1b03a94120d578f49e644
baileicheng <baileicheng@exfresh.com.cn>
2017-12-12 16:42:14 +0800

    [mod] init migrate

--- chzhshch/chzhshch/urls.py
+++ chzhshch/chzhshch/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    path('stock/', include('stock.urls')),
+    path('admin/', admin.site.urls),
 ]
